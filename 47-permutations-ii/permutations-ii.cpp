58b3a1f89feeb1e6256bbcc0a699f2b0976cd190
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-05-16 20:24:00 +0530

    Time: 7 ms (58.97%) | Memory: 9.8 MB (93.71%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-void solve(vector<int>nums, vector<vector<int>>&ans, int index){
-        if(index>=nums.size()){
-            if(find(ans.begin(), ans.end(), nums)!= ans.end()){return;}
-            ans.push_back(nums);
+        void solve(vector<int>&nums,vector<int>&arr,vector<vector<int>>&ans,vector<int>&selected){
+        if(arr.size()==nums.size()){
+            ans.push_back(arr);
             return;
         }
-
-        for(int j=index; j<nums.size();j++){
-            swap(nums[j], nums[index]);
-            solve(nums,ans,index+1);
-            swap(nums[j], nums[index]);
-        }
+        for(int i=0;i<nums.size();i++){
+                if(i>0 && nums[i]==nums[i-1] && selected[i-1]==0)continue;
+	            if(selected[i]==0){
+	                arr.push_back(nums[i]);
+	                selected[i]=1;
+	                solve(nums,arr,ans,selected);
+	                arr.pop_back();
+	                selected[i]=0;
+	            }
+	    }
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        int index=0;
-        solve(nums, ans, index);
-
-        return ans;
+        vector<int>selected(nums.size(),0);
+		vector<vector<int>>ans;
+		vector<int>arr;
+        sort(nums.begin(),nums.end());
+		solve(nums,arr,ans,selected);
+		return ans;
     }
 };
