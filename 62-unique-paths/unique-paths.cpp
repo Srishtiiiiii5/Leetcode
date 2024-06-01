5b080be11f3131d25d2909fed87dfbc44246b675
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-06-01 10:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (62.86%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -18,4 +18,16 @@ public:
         vector<vector<int>>dp(m, vector<int>(n,-1));
         return solve(m-1,n-1, dp);
     }
+    // int solve(int i, int j){
+    //     if (i==0 && j==0){
+    //         return 1;
+    //     }
+    //     if(i<0 || j<0){
+    //         return 0;
+    //     }
+    //     return solve(i-1,j)+ solve(i,j-1);
+    // }
+    // int uniquePaths(int m, int n) {
+    //     return solve(m-1,n-1);
+    // }
 };
