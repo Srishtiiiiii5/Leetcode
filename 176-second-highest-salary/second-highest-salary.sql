92a7e122154ad5593b76d4d803cd8dec7ca94eec
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-07-31 17:00:43 +0530

    Time: 243 ms (58.74%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
-select IFNULL(max(salary), null) as SecondHighestSalary
-from Employee
-where salary not in (
-    select max(salary)
-    from Employee
+-- select IFNULL(max(salary), null) as SecondHighestSalary
+-- from Employee
+-- where salary not in (
+--     select max(salary)
+--     from Employee
+-- )
 
-)
+
+-- SELECT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary < (SELECT MAX(salary)FROM Employee);
+
+select IFNULL((select distinct salary
+from Employee
+order by salary desc
+limit 1
+offset 1), null) as SecondHighestSalary
+
