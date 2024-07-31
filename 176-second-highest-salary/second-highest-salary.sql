4f67341642f1ecfb60c8ccde7bc096d209347a87
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-07-31 21:39:44 +0530

    Time: 262 ms (38.39%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -5,9 +5,10 @@
 --     select max(salary)
 --     from Employee
 -- )
-select IFNULL(
-(select distinct salary 
+-- 
+
+
+select max(salary) as SecondHighestSalary
 from Employee
-order by salary desc
-limit 1
-offset 1), null) as SecondHighestSalary
+where salary < (select max(salary) from Employee)
+
