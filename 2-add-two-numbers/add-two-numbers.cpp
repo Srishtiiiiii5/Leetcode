02bf55b26ec6954319a09b0b394f73caf28515af
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-08-11 07:59:53 +0530

    Time: 19 ms (73.61%) | Memory: 77.6 MB (22.46%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,119 +11,76 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* p;
-        ListNode* q;
-        ListNode* root;
-        ListNode* r;
-        p=l1;
-        q=l2;
+        ListNode* first= l1;
+        ListNode* second = l2;
+        ListNode* head = NULL;
+        ListNode* p= NULL;
         int carry=0;
-        int sum= p->val + q->val;
-        if(sum>9){
-            root=new ListNode(sum%10);
-            carry=sum/10;
-            root->next=NULL;
-        }
-        else{
-            root=new ListNode(sum);
-            root->next=NULL;
-        }
-        r=root;
-        p=p->next;
-        q=q->next;
-        while(p && q){
-            sum= p->val + q->val + carry;
-            if(sum>9){
-                ListNode* temp;
-                temp=new ListNode(sum%10);
-                temp->next=NULL;
-                r->next=temp;
-                r=temp;
-                carry=sum/10;
-                
-            }
-            else{
-                ListNode* temp;
-                temp=new ListNode(sum);
-                carry=0;
-                temp->next=NULL;
-                r->next=temp;
-                r=temp;
-            }
-            p=p->next;
-            q=q->next;
-        }
-        while(p){
-            ListNode* temp;
-            sum= p->val+ carry;
-            if(sum>9){
-                temp=new ListNode(sum%10);
-                carry=sum/10;
+        bool flag=true;
 
-            }
-            else{
-                temp=new ListNode(sum);
-                carry=0;
-            }
-            temp->next=NULL;
-            r->next=temp;
-            r=temp;
-            p=p->next;
-        }
-        while(q){
-            ListNode* temp;
-            sum=q->val+carry;
+        while(first && second){
+            int sum= first->val + second->val + carry;
             if(sum>9){
-                temp=new ListNode(sum%10);
-                carry=sum/10;
-
+                carry= 1;
+                sum= sum%10;
             }
             else{
-                temp=new ListNode(sum);
                 carry=0;
             }
+            ListNode* temp = new ListNode();
+            temp->val = sum;
+            if(flag){
+                head= temp;
+                p=head;
+                flag=false;
+            }
+            else{
+                p->next=temp;
+                p=temp;
+            }
             temp->next=NULL;
-            r->next=temp;
-            r=temp;
-            q=q->next;
+            first= first->next;
+            second = second->next;
         }
-        if(carry!=0){
-            ListNode* temp;
-            temp=new ListNode(1);
+        while(first ){
+            int sum= first->val + carry;
+            if(sum>9){
+                carry= 1;
+                sum= sum%10;
+            }
+            else{
+                carry=0;
+            }
+            ListNode* temp = new ListNode();
+            temp->val = sum;
             temp->next=NULL;
-            r->next=temp;
-            r=temp;
+            p->next= temp;
+            p=temp;
+            first= first->next;
         }
-        return root;
+        while(second){
+            int sum= second->val + carry;
+            if(sum>9){
+                carry= 1;
+                sum= sum%10;
+            }
+            else{
+                carry=0;
+            }
+            ListNode* temp = new ListNode();
+            temp->val = sum;
+            temp->next=NULL;
+            p->next= temp;
+            p=temp;
+            second= second->next;
+        }
+        if(carry== 1){
+            ListNode* temp = new ListNode();
+            temp->val = 1;
+            temp->next=NULL;
+            p->next= temp;
+        }
+        return head;
     }
-};
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2,int carry=0) {
 
-//         ListNode* n=new ListNode();
-//         if(l1 and l2){
-//             int num=l1->val + l2->val + carry;
-//             carry=num/10;
-//             num=num%10;
-//             n->val=num;
-//             n->next=addTwoNumbers(l1->next,l2->next,carry);
-//         }else if(l1){
-//             int num=l1->val + carry;
-//             carry=num/10;
-//             num=num%10;
-//             n->val=num;
-//             n->next=addTwoNumbers(l1->next,l2,carry);
-//         }else if(l2){
-//             int num=l2->val + carry;
-//             carry=num/10;
-//             num=num%10;
-//             n->val=num;
-//             n->next=addTwoNumbers(l1,l2->next,carry);
-//         }else if(carry>0){
-//             n->val=carry;
-//         }else return nullptr;
-        
-//         return n;
-//     }
-// };
+};
