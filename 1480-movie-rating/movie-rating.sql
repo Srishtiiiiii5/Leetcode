a4a78da44361899b28b957d30b8299d1c53c8fd6
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-09-13 20:45:30 +0530

    Time: 1067 ms (78.49%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,41 +1,19 @@
 # Write your MySQL query statement below
-/*
-select name as results
-from Users
-where user_id= (
-    select user_id
-    from MovieRating
-    group by user_id
-    order by count(*) desc, name asc
-    limit 1
-))
-union all
-(
-select title as results
-from Movies
-where movie_id = (
-    select movie_id
-    from MovieRating
-    where (created_at between '2020-02-01' and '2020-02-29')
-    group by movie_id
-    order by avg(rating) desc, title asc
-    limit 1
+#QUERY 1
+(select name as results
+from users inner join MovieRating using(user_id)
+group by user_id
+order by count(rating) desc, name asc
+limit 1
 )
+
+UNION all
+
+(select title as results
+from movies inner join MovieRating using(movie_id)
+where month(created_at)= '02' and year(created_at) = '2020'
+group by movie_id 
+order by avg(rating) desc, title
+limit 1
+
 )
-*/
-(SELECT name AS results
-FROM Users
-INNER JOIN MovieRating USING(user_id)
-GROUP BY user_id
-ORDER BY COUNT(rating) DESC, name
-LIMIT 1)
-
-UNION ALL
-
-(SELECT title AS results
-FROM Movies
-INNER JOIN MovieRating USING(movie_id)
-WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
-GROUP BY title
-ORDER BY AVG(rating) DESC, title
-LIMIT 1)
