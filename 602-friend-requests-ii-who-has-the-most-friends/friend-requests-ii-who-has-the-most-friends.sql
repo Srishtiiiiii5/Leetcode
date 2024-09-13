3b942bd98a6f1225a776cf13a1ef1d47ceeff7be
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-09-13 21:50:13 +0530

    Time: 323 ms (33.21%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,14 @@
-select requester_id as id,
-    (select count(*) from RequestAccepted  where (requester_id = id or accepter_id = id)) as num
+# Write your MySQL query statement below
+select requester_id as id, ( select count(*) from RequestAccepted 
+where (requester_id= id or accepter_id=id)) as num
 from RequestAccepted
 group by requester_id
-union 
-select accepter_id as id,
-    (select count(*) from RequestAccepted  where (requester_id = id or accepter_id = id)) as num
+
+union
+select accepter_id as id, (select count(*) from RequestAccepted 
+where (requester_id= id or accepter_id=id)) as num
 from RequestAccepted
 group by accepter_id
-order by num desc limit 1;       
+
+order by num desc
+limit 1
