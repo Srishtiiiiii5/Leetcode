d785c0e80b3ba9636d0e36529f9d0d1438867521
Srishti Gupta <97932072+Srishtiiiiii5@users.noreply.github.com>
2024-09-17 20:47:36 +0530

    Time: 491 ms (75.38%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,18 +1,11 @@
--- # Write your MySQL query statement below
--- select employee_id , department_id
--- from (
---     select employee_id, department_id, primary_flag, count(*) as count
---     from Employee
---     group by employee_id
--- ) AS subquery
--- where count=1 or primary_flag='Y';
+# Write your MySQL query statement below
+select employee_id, department_id
+from employee
+where primary_flag='Y'
 
-SELECT employee_id, department_id
-FROM Employee 
-WHERE primary_flag = 'Y'
 UNION
-SELECT employee_id, department_id
-FROM Employee 
-GROUP BY employee_id
-HAVING COUNT(employee_id) = 1
-ORDER BY employee_id;
+
+select employee_id, department_id
+from employee
+group by employee_id
+having count(employee_id)=1
